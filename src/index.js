1bfeb0e1c0aad4676efbe7c1ff0ec79e3b2fb6b0
Fahad Akram <fadiii3337@gmail.com>
2022-11-29 00:07:33 +0500

    added basename for hashrouter

--- src/index.js
+++ src/index.js
@@ -9,10 +9,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </BrowserRouter>
+    <HashRouter basename='/'>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
