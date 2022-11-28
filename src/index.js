8aace3f65be6159690f59e3026c3fa5931719a82
Fahad Akram <fadiii3337@gmail.com>
2022-11-28 23:59:53 +0500

    added hash router to make the app deploy on github correctly

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 // styles
 import './index.css';
 // components
@@ -10,7 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </BrowserRouter>
   </React.StrictMode>
 );
